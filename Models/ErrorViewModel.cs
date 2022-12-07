bc4dba3b98bd78229b6b67fa2ba47724b8f43aa5
lucianobritodev <lucianobrito.dev@gmail.com>
2022-12-07 12:32:30 -0300

    Created Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace workshop_asp_net_core_mvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
